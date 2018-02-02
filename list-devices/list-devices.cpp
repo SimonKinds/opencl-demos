44189b8e3999613460fa3facb25c5688c7571dcf
Simon Kindström <simon.kindstrom@gmail.com>
2018-02-02 16:12:09 +0100

    Print more device info

--- list-devices/list-devices.cpp
+++ list-devices/list-devices.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <sstream>
 
 #include "CL/cl2.hpp"
 
 std::vector<cl::Platform> getPlatforms();
-std::vector<cl::Device> getDevices(const cl::Platform&);
 std::string getPlatformName(const cl::Platform&);
-std::string getDeviceName(const cl::Device&);
+std::vector<cl::Device> getDevices(const cl::Platform&);
+std::string getDeviceInfo(const cl::Device&);
 
 int main() {
+  std::stringstream output;
   for (const auto& p : getPlatforms()) {
-    std::cout << "Platform: " << getPlatformName(p) << '\n';
-    std::cout << "Devices: ";
+    output << "Devices for platform: " << getPlatformName(p) << "\n\n";
     for (const auto& d : getDevices(p)) {
-      std::cout << getDeviceName(d) << ", ";
+      output << getDeviceInfo(d);
+      output << "\n\n";
     }
   }
-  std::cout << std::endl;
+  std::cout << output.str();
 }
 
 std::vector<cl::Platform> getPlatforms() {
@@ -33,6 +35,45 @@ std::vector<cl::Device> getDevices(const cl::Platform& platform) {
 std::string getPlatformName(const cl::Platform& platform) {
   return platform.getInfo<CL_PLATFORM_NAME>();
 }
-std::string getDeviceName(const cl::Device& device) {
-  return device.getInfo<CL_DEVICE_NAME>();
+std::string getDeviceInfo(const cl::Device& device) {
+  std::stringstream info;
+
+  info << "GENERAL";
+  info << "\nName: ";
+  info << device.getInfo<CL_DEVICE_NAME>();
+
+  info << "\nOpenCL version: ";
+  info << device.getInfo<CL_DEVICE_VERSION>();
+
+  info << "\nMax compute unit count: ";
+  info << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>();
+
+
+  info << "\n\nGLOBAL";
+  info << "\nglobal memory size (bytes): ";
+  info << device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>();
+
+  info <<  "\nglobal memory cache size (bytes): ";
+  info << device.getInfo<CL_DEVICE_GLOBAL_MEM_CACHE_SIZE>();
+
+  info <<  "\nglobal memory cache line size (memory per fetch) (bytes): ";
+  info << device.getInfo<CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE>();
+
+  info <<  "\nglobal memory cache line size (memory per fetch) (bytes): ";
+  info << device.getInfo<CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE>();
+
+  info << "\n\nLOCAL";
+  info << "\nlocal memory size (bytes): ";
+  info << device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>();
+
+  info << "\nlocal memory type: ";
+  info << (device.getInfo<CL_DEVICE_LOCAL_MEM_TYPE>() == CL_LOCAL
+    ? "dedicated local memory" : "shared with global memory");
+
+  info << "\n\nWORK GROUPS";
+  info << "\nMax work group size: ";
+  info << device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>();
+
+
+  return info.str();
 }
